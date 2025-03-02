4e868cbf4863eb8e4fc713d9d6d796c64d71419d
qwissx <borutenkobogdan@gmail.com>
2025-03-02 13:48:08 +0300

    deploy railway

--- bot/settings.py
+++ bot/settings.py
@@ -1,15 +1,16 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
-    bot_token: str
-    openai_api_key: str
+    bot_token: str = Field(..., env="BOT_TOKEN")
+    openai_api_key: str = Field(..., env="OPENAI_API_KEY")
 
-    db_name: str
-    db_user: str
-    db_port: str
-    db_host: str
-    db_pass: str
+    db_name: str = Field(..., env="DB_NAME")
+    db_user: str = Field(..., env="DB_USER")
+    db_port: str = Field(..., env="DB_PORT")
+    db_host: str = Field(..., env="DB_HOST")
+    db_pass: str = Field(..., env="DB_PASS")
 
     class Config:
         env_file = ".env"
